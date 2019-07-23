3757600e71884b6027f1b79843a4913bd9e6c98b
Nishant Yadav <nynishuyadav85@gmail.com>
2019-07-23 18:22:52 +0530

    Final changes

--- New.java
+++ New.java
@@ -2,7 +2,7 @@ class New
 {
   public static void main (String [] args)
   {
-    System.out.println("Its Good.!!!!!")
-      System.out.println("Final Word")
+    System.out.println("Its Good.!!!!!");
+      System.out.println("Final Word");
   }
 }
